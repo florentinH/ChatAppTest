cc3f06056562fa4fb5e71d3a5cee713c3ae20c35
florentinH <flokri14@gmail.com>
2018-09-04 00:02:25 +0200

    change of the css

--- chat-app-test/src/components/chats/SideBar.js
+++ chat-app-test/src/components/chats/SideBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import FAChevronDown from 'react-icons/lib/md/keyboard-arrow-down'
 import FAMenu from 'react-icons/lib/fa/list-ul'
-import FASearch from 'react-icons/lib/fa/search'
 import MdEject from 'react-icons/lib/md/eject'
 
 export default class SideBar extends Component{
@@ -11,15 +10,10 @@ export default class SideBar extends Component{
 		return (
 			<div id="side-bar">
 					<div className="heading">
-						<div className="app-name">Our Cool Chat <FAChevronDown /></div>
+						<div className="app-name">Our chat <FAChevronDown /></div>
 						<div className="menu">
 							<FAMenu />
 						</div>
-					</div>
-					<div className="search">
-						<i className="search-icon"><FASearch /></i>
-						<input placeholder="Search" type="text"/>
-						<div className="plus"></div>
 					</div>
 					<div 
 						className="users" 
